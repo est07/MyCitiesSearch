0a2b8e1f3f5c368b540bd9f591558ff7b5fc04f6
Esteban Serrano Vivas <est.2207@gmail.com>
2025-07-10 12:17:33 -0500

    Add ktlint plugin configuration for code style enforcement.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,22 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    ktlint {
+        android.set(true)
+        verbose.set(true)
+        ignoreFailures.set(true)
+        filter {
+            exclude {
+                it.file.path.contains("generated/") ||
+                        it.file.path.contains("test/") ||
+                        it.file.path.contains("androidTest/")
+            }
+        }
+    }
 }
